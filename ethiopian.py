645d63788deef1bf8814bf604cc6e4e1e1941ee2
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-12 09:31:45 +0900

    feat: Modify ethiopian.py
    Add features on ethiopian.py

--- ethiopian.py
+++ ethiopian.py
@@ -1,5 +1,12 @@
-def hello():
-    print('Good Luck!')
+def ethiopian(x, y):
+    _dict = {}
+    while x != 1:
+        x //= 2
+        y *= 2
+        if x % 2 != 0:
+            _dict[x] = y
+    value = list(_dict.values())
+    return sum(value)
 
 if __name__ == '__main__': # main 파일 내에서만 작동
-    hello()
+    print(ethiopian(14, 7))
